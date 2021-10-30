186c5b3c042d7d50f1c4b71d5c6189169046c324
Stollaz <stollerymatthew@gmail.com>
2021-10-30 15:12:06 +0100

    enemysun now follows player and will jump to get to player

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,11 +6,19 @@ public class Enemy : MonoBehaviour
 {
     public int health;
     public int maxHealth = 100;
+    Rigidbody rb;
+    public int maxJumps = 2;
+    int remainingJumps;
+
+    public Transform player;
 
     // Start is called before the first frame update
     void Start()
     {
+        rb = GetComponent<Rigidbody>();
         health = maxHealth;
+        Physics.gravity *= 2f;
+        remainingJumps = maxJumps;
     }
 
     public void TakeDamage(int amount) {
@@ -18,6 +26,30 @@ public class Enemy : MonoBehaviour
         if (health == 0) {
             Die();
         }
+    }
+
+    void Update(){
+        GoToPlayer(0.1f);
+        if (rb.velocity.y < -0.5f) Jump();
+    }
+
+    void GoToPlayer(float s){
+        Vector3 direction = new Vector3(transform.position.x - player.position.x, 0, transform.position.z - player.position.z).normalized;
+        rb.MovePosition(transform.position - s*direction);
+    }
+
+    void Jump(){
+        if (remainingJumps > 0){
+            Vector3 vel = rb.velocity;
+            vel.y = 0f;
+            rb.velocity = vel;
+            rb.AddForce(Vector3.up * 60000f);
+            remainingJumps--;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision) {
+        remainingJumps = maxJumps;
     }
 
     void Die() {
