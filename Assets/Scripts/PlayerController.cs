faf7ddb2c52482e714d5a2238abe7741fc4d8da5
Matt <matthew.crees@ntlworld.com>
2021-10-30 22:14:12 +0100

    lmao you died

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
@@ -76,6 +77,7 @@ public class PlayerController : MonoBehaviour
 
     void Die() {
         Debug.Log("lmao you died");
+        SceneManager.LoadScene("GameOver");
     }
 
 }
