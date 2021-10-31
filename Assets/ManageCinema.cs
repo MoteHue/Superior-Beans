3078c3b7dcf664a0d01ec0a69d1a73cadf086c60
Matt <matthew.crees@ntlworld.com>
2021-10-31 09:49:20 +0000

    subtitles

--- Assets/ManageCinema.cs
+++ Assets/ManageCinema.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 
 public class ManageCinema : MonoBehaviour
 {
-    Text uitext;
+    public Text uitext;
     public CinemaTime ct;
 
     private void Start() {
@@ -14,13 +14,28 @@ public class ManageCinema : MonoBehaviour
 
     IEnumerator doCinema() {
         ct.ChangeCamera(0);
+        StartCoroutine(fillInText("The Tower..."));
         yield return new WaitForSecondsRealtime(3);
         ct.ChangeCamera(1);
+        StartCoroutine(fillInText("Hair falls down..."));
         yield return new WaitForSecondsRealtime(3);
         ct.ChangeCamera(2);
+        StartCoroutine(fillInText("And we all know what hair means..."));
         yield return new WaitForSecondsRealtime(6);
         ct.ChangeCamera(3);
+        StartCoroutine(fillInText("A princess!"));
         yield return new WaitForSecondsRealtime(6);
+        StartCoroutine(fillInText("Hold on a minute..."));
         ct.ChangeCamera(4);
+    }
+
+    IEnumerator fillInText(string text) {
+        uitext.text = ""; 
+        int counter = 0;
+        while (counter < text.Length) {
+            yield return new WaitForSecondsRealtime(0.1f);
+            uitext.text += text[counter];
+            counter++;
+        }
     }
 }
